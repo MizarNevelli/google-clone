271526d4c7b350bb2d1139d22191a72750d6dd11
Mizar Nevelli <mizarnevelli@gmail.com>
2022-11-22 15:03:15 +0100

    improve:layout

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import Search from "../icons/Search";
+import { MicrophoneIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
   return (
@@ -12,10 +13,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <form>
-        <div className="w-full flex justify-center">
-          <Image alt="logo" width={300} height={100} src="/google.jpeg" />
+      <form className="w-full flex flex-col items-center mt-36 ">
+        <Image alt="logo" width={300} height={100} src="/google.jpeg" />
+        <div
+          className="
+          flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 hover:shadow-lg 
+          focus-within:shadow-lg px-5 py-3 rounded-full items-center sm:max-w-xl lg:max-w-2xl"
+        >
           <Search />
+          <input type="text" className="flex-grow focus:outline-none " />
+          <MicrophoneIcon className="w-6 h-6 text-gray-500" />
+        </div>
+        <div className="flex flex-col sm:flex-row w-1/2 space-y-2 mt-8 sm:space-y-0 sm:space-x-4 sm:justify-center">
+          <button className="btn">Google Search</button>
+          <button className="btn">I'm feeling lucky</button>
         </div>
       </form>
     </div>
