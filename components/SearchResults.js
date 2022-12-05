c881fd6377cc7dcf98bc267d5b69f1fc3fd19d83
Mizar Nevelli <mizarnevelli@gmail.com>
2022-12-05 15:00:57 +0100

    complete-result-section

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,12 +1,31 @@
-import { useRouter } from "next/router";
-
 export default function SearchResults({ results }) {
   return (
     <div className="w-full mx-auto px-3 sm:pl[5%] md:pl-[14%] lg:pl-52">
       <p className="text-gray-600 text-sm mb-5 mt-3">
         About {results.searchInformation.formattedTotalResults} results (
-        {results.searchInformation.searchTime} seconds)
+        {results.searchInformation.searchTime.toFixed(3)} seconds)
       </p>
+      {results.items.map((item) => (
+        <div key={item.link} className="max-w-xl mb-8 ">
+          <div className="group ">
+            <a className="text-sm truncate" href={item.link}>
+              {item.formattedUrl}
+            </a>
+            <a
+              className="group-hover:underline decoration-blue-800"
+              href={item.link}
+            >
+              <h2 className="truncate text-xl font-medium text-blue-800">
+                {item.title}
+              </h2>
+            </a>
+          </div>
+          <p
+            className="text-gray-600"
+            dangerouslySetInnerHTML={{ __html: item.htmlSnippet }}
+          />
+        </div>
+      ))}
     </div>
   );
 }
